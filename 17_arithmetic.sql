9cd008f1b8728e1364aebc71c998814e2e5bf8f6
ilante <44288547+ilante@users.noreply.github.com>
2022-06-16 11:39:49 +0200

    Revenue

---
+++ 17_arithmetic.sql
@@ -0,0 +1,13 @@
+-- How many times has each movie been rented out
+-- How much money has each movie generated (revenue = rate * times rented "price x quantity")
+
+SELECT
+    f.title as "Movie Title", f.rental_rate as "Rental Rate", count(r.rental_id) as "Times Rented out", f.rental_rate * count(r.rental_id) as Revenue
+FROM
+    film f, rental r, inventory i
+WHERE
+    i.inventory_id = r.inventory_id
+    AND
+    f.film_id = i.film_id
+GROUP BY 1 
+;
